a9cd37826250a8fd58a82b66a109072543e112f2
John Kollman <johnkollman623@gmail.com>
2022-05-02 12:27:29 -0400

    minor

--- socketUtils/serverSide/base.py
+++ socketUtils/serverSide/base.py
@@ -11,15 +11,15 @@ class Base:
 
         while True:
             print('Waiting for client to connect')
-            clientsocket, address = s.accept()
+            clientsocket, address = self.s.accept()
             print(f"Connection from {self.ip} has been established!")
             clientsocket.send(bytes("Welcome to the server!", "utf-8"))
     
     # method to keep connecting method consistent 
     def socketConnect(self):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind(('',self.port))
-        s.listen(5)
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.bind(('',self.port))
+        self.s.listen(5)
         
 if __name__ == '__main__':
     b = Base()
